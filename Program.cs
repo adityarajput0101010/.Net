14803ccf40f74a7819ee13912981176a8b851aac
adityarajput0101010 <adityarajput0101010@gmail.com>
2026-04-23 14:23:38 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Threading.Tasks;
+var builder = WebApplication.CreateBuilder(args);
 
-class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    static async Task Main()
-    {
-        Console.WriteLine("Task Started");
-
-        await Work();
-
-        Console.WriteLine("Task Completed");
-    }
-
-    static async Task Work()
-    {
-        await Task.Delay(3000);
-        Console.WriteLine("Working...");
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
