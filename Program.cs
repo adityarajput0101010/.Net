3de4806ded837cf74612300f682e10c88dd8c639
adityarajput0101010 <adityarajput0101010@gmail.com>
2026-04-23 14:18:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 20;
+        int b = 10;
+
+        Console.WriteLine("First Number: " + a);
+        Console.WriteLine("Second Number: " + b);
+
+        Console.WriteLine("Addition = " + (a + b));
+        Console.WriteLine("Subtraction = " + (a - b));
+        Console.WriteLine("Multiplication = " + (a * b));
+        Console.WriteLine("Division = " + (a / b));
+    }
+}
